ec0db0c51c1e17a82fcaa67fab3c161b0c851c77
Benjamin R. Straw <benrstraw@gmail.com>
2017-10-08 14:03:27 -0400

    Backend nearing finalization for Knight Hacks 2017.

--- backend/writewiththenet.py
+++ backend/writewiththenet.py
@@ -13,6 +13,8 @@ PORT_NUMBER = 8462
 
 MAX_LINES_PER_STORY = 25
 MAX_REQS_BEFORE_NEW = 10
+SEEN_LIVE_TIME = 0
+
 CURRENT_REQUESTS = 0
 
 stories = [
@@ -74,7 +76,6 @@ def go_get_story(self):
 
 	cursor.execute("SELECT line_text FROM story_lines WHERE story_id = %s", (get_vars['id'][0],))
 	thelines = cursor.fetchall()
-	print(thelines)
 	print(json.dumps(thelines))
 	resp = json.dumps(thelines)
 
@@ -87,6 +88,9 @@ def go_get_line(self):
 	self.send_response(200)
 	self.send_header("Content-Type", "application/json")
 
+	global CURRENT_REQUESTS
+	CURRENT_REQUESTS += 1
+
 	line_text = ""
 	lines_left = 999
 	story_id = -1
@@ -96,34 +100,58 @@ def go_get_line(self):
 		story_id = cursor.fetchone()[0] + 1
 
 		try:
-			cursor.execute("INSERT INTO story_lines (story_id, line_text) VALUES (%d,%s)", (story_id, line_text))
+			cursor.execute("INSERT INTO story_lines (story_id, line_text) VALUES (%s,%s)", (story_id, line_text))
 		except mariadb.Error as error:
 			print("Error: {}".format(error))
 			return
+		mariadb_connection.commit()
+		CURRENT_REQUESTS = 0
 	else:
 		#cursor.execute("SELECT line_text FROM story_lines WHERE last_seen < NOW() - INTERVAL 5 MINUTE ORDER BY ID DESC LIMIT 1")
 		cursor.execute("""SELECT * FROM
-			(SELECT * FROM story_lines WHERE line_id IN (SELECT MAX(line_id) FROM story_lines GROUP BY story_id) ORDER BY line_id DESC) AS last_lines
-			WHERE last_seen < NOW() - INTERVAL 5 MINUTE""")
+			(SELECT * FROM story_lines WHERE line_id IN (SELECT MAX(line_id) FROM story_lines GROUP BY story_id HAVING COUNT(*) <= %s) ORDER BY line_id DESC) AS last_lines
+			WHERE last_seen < NOW() - INTERVAL %s MINUTE""", (MAX_LINES_PER_STORY, SEEN_LIVE_TIME))
 
 		recents = cursor.fetchall()
 		if not recents:
-			self.send_response(500)
-			self.end_headers()
-			return
+			line_text = secrets.choice(stories)
+			cursor.execute("SELECT MAX(story_id) FROM story_lines")
+			last_story_id = cursor.fetchone()[0]
+			if last_story_id:
+				story_id = last_story_id + 1
+			else:
+				story_id = 1
 
-		cursor.execute("SELECT line_text, story_id FROM story_lines WHERE line_id = %s", (secrets.choice(recents)[0],))
+			try:
+				cursor.execute("INSERT INTO story_lines (story_id, line_text) VALUES (%s,%s)", (story_id, line_text))
+			except mariadb.Error as error:
+				print("Error: {}".format(error))
+				return
+			mariadb_connection.commit()
+		else:
+			new_line_id = secrets.choice(recents)[0]
+			cursor.execute("SELECT line_text, story_id FROM story_lines WHERE line_id = %s", (new_line_id,))
+			res = cursor.fetchone()
+			line_text = res[0]
+			story_id = int(res[1])
+			cursor.execute("UPDATE story_lines SET last_seen = NOW() WHERE line_id = %s", (new_line_id,))
+			mariadb_connection.commit()
 
-		res = cursor.fetchone()
-		line_text = res[0]
-		story_id = int(res[1])
 
 		cursor.execute("SELECT COUNT(story_id) FROM story_lines WHERE story_id = %s", (story_id,))
 		lines_left = MAX_LINES_PER_STORY - cursor.fetchone()[0]
 
 	self.end_headers()
 
-	resp = "{\"text\":\"" + line_text + "\",\"left\":" + str(lines_left) + ",\"story_id\":" + str(story_id) + "}"
+	kvjson = {}
+	kvjson["text"] = line_text
+	kvjson["left"] = lines_left
+	kvjson["story_id"] = story_id
+
+	resp = json.dumps(kvjson)
+	print(resp)
+
+	#resp = "{\"text\":\"" + line_text + "\",\"left\":" + str(lines_left) + ",\"story_id\":" + str(story_id) + "}"
 
 	self.wfile.write(resp.encode())
 	return
@@ -170,6 +198,8 @@ if __name__ == '__main__':
 		pass
 	
 	httpd.server_close()
+
+	mariadb_connection.commit()
 	mariadb_connection.close()
 
 	print(time.asctime(), 'Server Stops - %s:%s' % (HOST_NAME, PORT_NUMBER))
